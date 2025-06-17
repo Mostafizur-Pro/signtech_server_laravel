3748b5fe32c341afd228e30d58307e689b8c0aae
mostafizur-pro <mostafizur0195@gmail.com>
2025-06-17 18:05:51 +0600

    user show and update controller work

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/v1/register', [AuthController::class, 'register']);
 
 Route::get('/v1/user', [AuthController::class, 'index']);
+Route::get('/v1/user/{id}', [AuthController::class, 'show']);
 Route::delete('/v1/user/{id}', [AuthController::class, 'destroy']);
+Route::put('/v1/user/{id}', [AuthController::class, 'update']);
