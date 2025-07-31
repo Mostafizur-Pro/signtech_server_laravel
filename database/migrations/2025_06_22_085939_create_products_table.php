ff85d8df3a1c1c53da9220d8e7e27e2ca67a6c32
mostafizur-pro <mostafizur0195@gmail.com>
2025-07-31 18:28:59 +0600

    update

--- database/migrations/2025_06_22_085939_create_products_table.php
+++ database/migrations/2025_06_22_085939_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('model');
+            $table->string('title');
             $table->string('cooling_kw');
             $table->integer('cooling_btu');
             $table->string('cooling_tr');
